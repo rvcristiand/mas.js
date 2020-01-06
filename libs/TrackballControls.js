efcdac486a603d99ad5ed1df0abb3d21544f7777
rvcristiand <rvcristiand@gmail.com>
2020-01-06 12:52:43 -0500

    Z axis upwards always

--- libs/TrackballControls.js
+++ libs/TrackballControls.js
@@ -193,7 +193,7 @@ THREE.TrackballControls = function ( object, domElement ) {
 				quaternion.setFromAxisAngle( axis, -angle );
 
 				_eye.applyQuaternion( quaternion );
-				_this.object.up.applyQuaternion( quaternion );
+				// _this.object.up.applyQuaternion( quaternion );
 
 				_rotateEnd.applyQuaternion( quaternion );
 
