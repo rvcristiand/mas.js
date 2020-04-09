e139aff30937b0674b6a8358de0b87f6dd2f5c28
rvcristiand <rvcristiand@unal.edu.co>
2020-04-09 13:50:44 -0500

    fancy console animation added

--- console.js
+++ console.js
@@ -1,16 +1,57 @@
-function run(command) {
-  if (event.keyCode == 13) {
-    alert(check_command(command.value));
+var command_line = document.getElementById("command-line");
+var message = document.getElementById("message");
+
+function run_command(event) {
+  if (event.key == 'Enter') {
+    var command = command_line.value;
+
+    message.classList.add("remove-message")
+    message.classList.remove("message-successful", "message-error");
+
+    if (check_command(command)) {
+      message.classList.add("message-successful");
+      message.innerHTML = command;
+    } else {
+      message.classList.add("message-error");
+      message.innerHTML = "SyntaxError: invalid syntax";
+    }
+
+    command_line.value = "";
+
+    message.style.animation = 'none';
+    message.offsetHeight; /* trigger reflow */
+    message.style.animation = null; 
   }
 }
 
 function check_command(command) {
     var pattern = /^[_a-z]\w*\([\w,\s]*\)$/i;
     return pattern.test(command);
-  }
+}
+
+function show_message() {
+  message.style.opacity = 1;
+  
+  message.style.animation = 'none';
+}
+
+function hide_message() {
+  message.classList.add("remove-message");
+  message.offsetHeight; /* trigger reflow */
+  message.style.animation = null;
+}
+
+function end_animation() {
+  message.classList.remove("remove-message");
+  message.style.opacity = 0;
+}
 
 function remove_focus() {
-  var command_line = document.getElementById("command-line");
-
   command_line.blur();
 }
+
+// function give_focus() {
+//   console.log("holi");
+// }
+
+
