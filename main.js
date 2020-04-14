a5c43d2fa8e1059863dbbab1c68baa9977c14fd6
rvcristiand <rvcristiand@unal.edu.co>
2020-04-14 18:29:47 -0500

    autocomplete added

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import * as console from "./modules/console.js";
 import * as FEM from "./modules/FEM.js";
 
-var command_line = document.getElementById("command-line");
 var output = document.getElementById("output");
 var log = document.getElementById("log");
+var command_line = document.getElementById("command-line");
+var autocomplete = document.getElementById("autocomplete");
 
 // set the functions
 console.add_function("loadModel", {
@@ -17,12 +18,26 @@ console.add_function("loadModel", {
 });
 
 // set events
-document.addEventListener("keydown", console.give_focus);
-document.addEventListener("mousedown", console.remove_focus);
-
-command_line.addEventListener("change", console.run_command);
+document.addEventListener("keypress", console.give_focus);
+// document.addEventListener("mousedown", console.remove_focus);
 
 output.addEventListener("mouseover", console.show_message);
 output.addEventListener("mouseout", console.hide_message);
 
-log.addEventListener("animationend", console.end_animation);
+log.addEventListener("animationend", console.stop_animation);
+
+command_line.addEventListener("keydown", console.run_command);  // enter and option_selected === -1
+command_line.addEventListener("keydown", console.select_option);  // enter and option_selected !=== -1
+command_line.addEventListener("keydown", console.esc);  // esc
+
+command_line.addEventListener("input", console.show_autocomplete); // show autocomplete
+command_line.addEventListener("keydown", console.choose_option);  // tab
+
+// autocomplete.addEventListener("mouseover", console.remove_selection);
+
+// disable tab
+document.onkeydown = function (event) {
+  if ( event.key == 'Tab' ) {
+    return false;
+  }
+}
--- modules/console.js
+++ modules/console.js
@@ -1,73 +1,177 @@
 var functions = {};
+var functions_name = ['loadModel','loadFile'];
+
+var option_selected = -1;
 
 var command_line = document.getElementById("command-line");
-var message = document.getElementById("log");
+var log = document.getElementById("log");
+var autocomplete = document.getElementById("autocomplete");
+
 
 export function run_command(event) {
-  // run the command
+  // run command
 
-  // command
-  var command = event.target.value;
+  if ( (event.key == 'Enter') && (option_selected === -1) ) {
+    var command = event.target.value;
 
-  // add class
-  message.classList.add("remove-message")
-  // remove classes
-  message.classList.remove("message-successful", "message-error");
+    log.classList.add("remove-message")
+    log.classList.remove("message-successful", "message-error");
 
-  // check if command is valid
-  if (check_command(command)) {
-    // function name
-    var f = get_name_function(command);
-    
-    // check if function name is a valid function
-    if (f in functions) {
-      // get parameters
-      var params = get_param_function(command);
+    log.style.visibility = "visible";
 
-      functions[f]['func'].apply(null, params)
-        .then(function () {
-          // command is valid
-          message.classList.add("message-successful");
-          message.textContent = functions[f]['successful'].apply(null, params);
-        })
-        .catch(function (e) {
-          // command is no valid 
-          message.classList.add("message-error");
-          message.textContent = e.name + ": " + e.message; // functions[f]['error'].apply(null, params);
-        });
+    // check if command is valid
+    if ( check_command(command) ) {
+      // get function name
+      var f = get_function_name(command);
+      
+      // check if function name is a valid function
+      if (f in functions) {
+        // get parameters
+        var params = get_param_function(command);
+
+        // run function
+        functions[f]['func'].apply(null, params)
+          .then(function () {
+            // command is valid
+            log.classList.add("message-successful");
+            log.textContent = functions[f]['successful'].apply(null, params);
+          })
+          .catch(function (e) {
+            // command is no valid 
+            log.classList.add("message-error");
+            log.textContent = e.name + ": " + e.message; // functions[f]['error'].apply(null, params);
+          });
+      } else {
+        // command doesnt exist
+        log.classList.add("message-error");
+
+        log.textContent = "'" + f + "'" + " is not recognized as a function";
+      }
     } else {
-      // command doesnt exist
-      message.classList.add("message-error");
+      // sintaxis error
+      log.classList.add("message-error");
 
-      message.textContent = "'" + f + "'" + " is not recognized as a command";
+      log.textContent = "SyntaxError: invalid syntax";
     }
-  } else {
-    // sintaxis error
-    message.classList.add("message-error");
-    message.textContent = "SyntaxError: invalid syntax";
+
+    // clear the command-line
+    command_line.value = "";
+
+    // resert animation
+    log.style.animation = 'none';
+    void(log.offsetHeight); /* trigger reflow */
+    log.style.animation = null;
+  }
+}
+
+export function show_autocomplete(event) {
+  // show autocomplete
+  
+  // remove previous autocomplete
+  while ( autocomplete.firstChild ) {
+    autocomplete.removeChild(autocomplete.lastChild);
   }
 
-  // clear the command-line
-  command_line.value = "";
+  var command = event.target.value;
+  // if no command exit
+  if ( command == "") {
+    return;
+  }
 
-  // resert animation
-  message.style.animation = 'none';
-  void(message.offsetHeight); /* trigger reflow */
-  message.style.animation = null;
+  var functions_name = get_functions_name();
+  // show the options
+  for ( const function_name of functions_name) {
+    if ( function_name.includes(command) ) {
+      var p = document.createElement("span");
+
+      p.addEventListener('click', option_clicked);
+      p.innerHTML = function_name;
+      p.classList.add("option");
+
+      autocomplete.appendChild(p);
+    }
+  }
+}
+
+export function choose_option(event) {
+  // choose option
+
+  if ( event.key == "Tab" ) {
+    command_line.focus();
+    var options = autocomplete.children;
+    var num_options = options.length;    
+
+    // remove any previous option selected
+    for ( var i = 0; i < num_options; i++ ) {
+      options[i].classList.remove("option-selected");
+    }
+
+    // select option
+    if ( !event.shiftKey ) {
+      if ( option_selected + 1 < num_options ) {
+        option_selected += 1;
+  
+        options[option_selected].classList.add("option-selected");
+      } else {
+        option_selected = -1;
+      }
+    } else {
+      if ( 0 < option_selected ) {
+        option_selected -= 1;
+  
+        options[option_selected].classList.add("option-selected");
+      } else {
+        if ( option_selected < 0 ) {
+          option_selected = num_options - 1;
+          options[option_selected].classList.add("option-selected");
+        } else {
+          option_selected = -1;
+        }
+      }      
+    }
+  }
+}
+
+export function select_option(event) {
+  // select option
+  if ( (event.key == "Enter") && (option_selected != -1) ) {
+    var options = autocomplete.children;
+    var option = options[option_selected].innerHTML;
+    
+    apply_option(option);
+  }
+}
+
+function option_clicked(event) {
+  apply_option(event.target.textContent);
+}
+
+function apply_option(option) {
+  // apply optiont selected
+  command_line.value = option + '()';
+  
+  option_selected = -1;
+
+  remove_options();
+  set_carret_position();
 }
 
 function check_command(command) {
+  // check if a command has the function's structure
     var pattern = /^[_a-z]\w*\([\w\s.,'"]*\)$/i;
+
     return pattern.test(command);
 }
 
-function get_name_function(command) {
+function get_function_name(command) {
+  // get the function's name 
   var pattern = /(^[_a-z]\w*)\([\w\s.,'"]*\)$/i;
 
   return command.match(pattern)[1];
 }
 
 function get_param_function(command) {
+  // get parameters function
   var pattern = /^[_a-z]\w*\(([\w\s.,'"]*)\)$/i;
   var params = command.match(pattern)[1].split(',');
   
@@ -83,30 +187,91 @@ function get_param_function(command) {
 }
 
 export function add_function(name, obj) {
+  // add funciton
+
   functions[name] = obj;
 }
 
+function get_functions_name() {
+  // get an array with all function's name registred through add_function
+  // var functions_name = [];
+  // const keys = Object.keys(functions);
+
+  // for ( const key of keys) {
+  //   functions_name.push(key);
+  // }
+
+  return functions_name;
+}
+
+function set_carret_position() {
+  // set carret position between parethesis
+  var position = command_line.value.length - 1;
+
+  if ( command_line.createTextRange ) {
+    var range = command_line.createTextRange();
+    range.move('character', position);
+  } else {
+    if ( command_line.selectionStart ) {
+      command_line.focus();
+      command_line.setSelectionRange(position, position);
+    } else {
+      command_line.focus();
+    }
+  }
+}
+
+function remove_options() {
+  // remove options
+
+  while (autocomplete.firstChild) {
+    autocomplete.removeChild(autocomplete.lastChild);
+  }
+
+  option_selected = -1;
+}
+
+export function remove_selection() {
+  // remove selection
+
+  var options = autocomplete.children;
+  var num_options = options.length;
+
+  for ( var i = 0; i < num_options; i++ ) {
+    options[i].classList.remove("option-selected");
+  }
+}
+
 export function show_message() {
-  message.style.opacity = 1;
-  
-  message.style.animation = 'none';
+  // show the message
+  log.classList.remove("remove-message");
+  log.style.visibility = "visible";
 }
 
 export function hide_message() {
-  message.classList.add("remove-message");
-  message.offsetHeight; /* trigger reflow */
-  message.style.animation = null;
+  // hide the message
+  log.classList.add("remove-message");
 }
 
-export function end_animation() {
-  message.classList.remove("remove-message");
-  message.style.opacity = 0;
+export function stop_animation() {
+  // stop animation
+  log.classList.remove("remove-message");
+  log.style.visibility = "hidden";
 }
 
-export function remove_focus() {
-  command_line.blur();
+export function esc(event) {
+  // remove focus to input when esc is pressed
+  if (event.key == 'Escape') {
+    remove_focus();
+  }
 }
 
 export function give_focus() {
+  // give focus to input
   command_line.focus();
+}
+
+export function remove_focus() {
+  // remove focus to input
+  command_line.blur();
 }
